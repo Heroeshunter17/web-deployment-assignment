c80b6ce257f6050a5ee54b2bae6831fa315cb03a
Fairuz Afnan <81913620+Heroeshunter17@users.noreply.github.com>
2022-12-16 12:11:31 +0700

    assignment

---
+++ frontend-deployment-assignment-v1/src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Fairuz Aniq Afnan" // TODO: replace this
+const studentId = "FE2685230" // TODO: replace this
+
+export { url, studentName, studentId }
